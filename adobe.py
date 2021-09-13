6c7d048217ca719505d4383ab88ebc2eda2f3be9
rayees09 <90627829+rayees09@users.noreply.github.com>
2021-09-13 07:28:46 -0700

    Adobe Technical Challenge - Python code

---
+++ adobe.py
@@ -0,0 +1,21 @@
+import urllib3, json
+
+http = urllib3.PoolManager()
+response = http.request('GET', 'https://api.github.com/orgs/adobe/repos')
+repo_list = json.loads(response.data.decode('UTF-8'))
+sorted_repo_list = sorted(repo_list, key=lambda k: k['updated_at'], reverse=True)
+print("-"*163)
+print ("{:<20} {:<32} {:<21} {:<17} {:<17} {:<23} {:<25} {:<23}".format( "| Repo ID", "Repo Name", " Size (MB)", "Stargazers", "No Forks ", "License", "Updated" , "|"))
+print("-"*163)
+for repo in sorted_repo_list:
+    repo_id = repo.get("id") 
+    repo_name = repo.get("name")
+    repo_size = repo.get("size")
+    stargazers_count = repo.get("stargazers_count")
+    forks = repo.get("forks")
+    updated_at = repo.get("updated_at")
+    license = "Not Available"
+    if(repo.get("license") != None) and (repo.get("license").get("name") != "Other"):
+        license = repo.get("license").get("name")
+    print ("{:<1} {:<18} {:<33} {:<20} {:<17} {:<17} {:<23} {:<25} {:<15}".format("|", repo_id, repo_name, repo_size, stargazers_count, forks, license, updated_at ,"|"))
+print("-"*163)
